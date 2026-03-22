5139fd0e1f4ffeb4f837da1a8c767ead8da17e69
Anand Raj <hcanand@msn.com>
2026-03-22 22:36:12 +0530

    Add SampleTests class with a basic sanity test

---
+++ templates/dotnet/tests/ProjectName.Tests/SampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ProjectName.Tests
+{
+    public sealed class SampleTests
+    {
+        [Fact]
+        public void Sanity()
+        {
+            Assert.True(true);
+        }
+    }
+}
